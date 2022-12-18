6299c1f2a7d772fc616bd34697e131afa61acc6d
anton <olivson@remed.care>
2022-12-18 07:08:18 +0200

    updated response format

--- backend/utils.py
+++ backend/utils.py
@@ -9,23 +9,24 @@ class Response:
     first_line: bytes
     headers: bytes
     body: bytes
+    separator: Optional[bytes] = b'\r\n'
 
     def send(self, writer: StreamWriter):
         writer.write(self.first_line)
         writer.write(self.headers)
-        writer.write(b'\n')
+        writer.write(self.separator)
         writer.write(self.body)
 
 
 def make_response(http_version, path, body: str, status: Optional[HTTPStatus] = HTTPStatus.OK) -> Response:
     body_len = len(body)
-    first_line = f'{http_version} {path} {status.value} {status.phrase}'.encode()
+    first_line = f'{http_version} {path} {status.value} {status.phrase}'.encode() + b'\r\n'
     headers = (
-        b'Content-Type: application/json\n'
-        b'Accept: application/json; encoding=utf8\n'
-        b'Connection: close\n'
-        b'Access-Control-Allow-Origin: *\n'
-        b'Access-Control-Allow-Methods: *\n'
-        b'Content-Length:' + f'{body_len}'.encode() + b'\n'
+        b'Content-Type: application/json\r\n'
+        b'Accept: application/json; encoding=utf8\r\n'
+        b'Connection: close\r\n'
+        b'Access-Control-Allow-Origin: *\r\n'
+        b'Access-Control-Allow-Methods: *\r\n'
+        b'Content-Length:' + f'{body_len}'.encode() + b'\r\n'
     )
     return Response(first_line=first_line, headers=headers, body=body.encode())
